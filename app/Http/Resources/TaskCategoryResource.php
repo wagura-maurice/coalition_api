e238c8e294a73cfd2f103dbe3ffbe5925b3e2c63
Wagura Maurice <business@waguramaurice.com>
2024-04-12 06:23:32 +0300

    feat: create TaskCatalogResource.php and TaskCategoryResource.php

---
+++ app/Http/Resources/TaskCategoryResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskCategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+    
+    public function with(Request $request)
+    {
+        return [
+            'throwable' => array_filter([
+                'status' => optional($request)->status,
+                'message' => optional($request)->message
+            ])
+        ];
+    }
+    
+    public function withResponse(Request $request, $response)
+    {
+        $response->header('Accept', 'application/json');
+    }
+}
