ccf9a5bb5f9d98354eaadc730bf1ba57c4d5458f
Wagura Maurice <business@waguramaurice.com>
2024-04-17 00:24:41 +0300

    feat: create TaskCatalogFactory.php

---
+++ database/factories/TaskCatalogFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TaskCatalog>
+ */
+class TaskCatalogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            '_uid' => generateUID(\App\Models\TaskCatalog::class, $this->faker->numberBetween(5, 9)),
+            'category_id' => \App\Models\TaskCategory::inRandomOrder()->value('id'),
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->unique()->slug(),
+            'description' => $this->faker->paragraph(),
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 year'),
+            '_priority' => $this->faker->numberBetween(0, 9),
+            '_status' => $this->faker->numberBetween(0, 4)
+        ];
+    }
+}
