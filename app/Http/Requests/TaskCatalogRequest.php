2cb52d9dec427e1a17caabdeda39916f5599d50f
Wagura Maurice <business@waguramaurice.com>
2024-04-17 05:10:56 +0300

    update TaskCatalogRequest.php

--- app/Http/Requests/TaskCatalogRequest.php
+++ app/Http/Requests/TaskCatalogRequest.php
@@ -41,30 +41,21 @@ class TaskCatalogRequest extends FormRequest
             $category = TaskCategory::find($this->category_id ?? $catalog->category_id);
 
             $this->merge(array_filter([
-                '_priority' => optional($this)->_priority ? $this->_priority : ($catalog->_priority ?? $this->generatePriorityId()),
+                '_priority' => optional($this)->_priority ? $this->_priority : ($catalog->_priority ?? null),
                 'due_date' => optional($this)->due_date ? Carbon::parse($this->due_date)->toDateTimeString() :  Carbon::now()->toDateTimeString(),
                 '_status' => optional($this)->_status ? $this->_status : ($catalog->_status ?? NULL)
             ]));
         } else {
             $category = TaskCategory::find($this->category_id);
-            $UUID = generateUID(TaskCatalog::class, 6);
+            $UUID = generateUID(TaskCatalog::class, rand(5, 9));
 
             $this->merge(array_filter([
                 '_uid' => "TASK/CATALOG/" . $UUID,
                 'slug' => strtoupper(Str::slug($this->title, '_') . '_' . $UUID . '_' . Carbon::now()->format('YmdHis')),
-                '_priority' => optional($this)->_priority ? $this->_priority : $this->generatePriorityId(),
+                '_priority' => optional($this)->_priority ? $this->_priority : null,
                 'due_date' => optional($this)->due_date ? Carbon::parse($this->due_date)->toDateTimeString() :  Carbon::now()->toDateTimeString(),
                 '_status' => optional($this)->_status ? $this->_status : NULL
             ]));
         }
-    }
-
-    private function generatePriorityId(): int
-    {
-        // Get the maximum priority ID from the task catalogs
-        $maxPriorityId = TaskCatalog::max('_priority');
-
-        // Increment the maximum priority ID to get the next available priority ID
-        return $maxPriorityId !== null ? $maxPriorityId + 1 : 1;
     }
 }
