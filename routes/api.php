96a14716bf1b0218640e3929f86d1a049f6b8f93
Wagura Maurice <business@waguramaurice.com>
2024-06-02 17:30:05 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskCatalogController;
 use App\Http\Controllers\TaskCategoryController;
+use App\Http\Controllers\TaskPriorityController;
 
-/* Route::get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum'); */
+})->middleware('auth:sanctum');
 
 Route::group(['prefix' => '/coalition'], function () {
     Route::apiResource('/task/category', TaskCategoryController::class, ['as' => 'task']);
+    Route::apiResource('/task/priority', TaskPriorityController::class, ['as' => 'task']);
     Route::apiResource('/task/catalog', TaskCatalogController::class, ['as' => 'task']);
 });
