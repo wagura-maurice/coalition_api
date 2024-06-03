390aa8d2781257d6035c1e3be3176ef5420691f3
Wagura Maurice <business@waguramaurice.com>
2024-06-03 13:13:33 +0300

    update 2024_04_11_142714_create_task_priorities_table.php

--- database/migrations/2024_04_11_142714_create_task_priorities_table.php
+++ database/migrations/2024_04_11_142714_create_task_priorities_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('task_priorities', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('color')->unique()->nullable();
             $table->text('description')->nullable();
             $table->softDeletes();
             $table->timestamps();
